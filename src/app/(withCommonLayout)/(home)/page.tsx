a168930296e534243898ba2f7b3e5cd09a151152
Hosain <cryptichasan925@gmail.com>
2025-08-04 21:46:54 +0600

    about sectoin done

--- src/app/(withCommonLayout)/(home)/page.tsx
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -1,5 +1,5 @@
-import AboutSection from "@/components/about/about-section";
-import Hero from "@/components/Hero/Hero";
+import AboutSection from "@/components/home/about/about-section";
+import Hero from "@/components/home/Hero/Hero";
 
 export default function Home() {
   return (
